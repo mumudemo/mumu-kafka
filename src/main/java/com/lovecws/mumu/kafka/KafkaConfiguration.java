b2eefe893f98b162ae79aa371356bc5631be6c95
baby慕慕 <lovercws@gmail.com>
2017-09-11 09:33:37 +0800

    travis构建

--- src/main/java/com/lovecws/mumu/kafka/KafkaConfiguration.java
+++ src/main/java/com/lovecws/mumu/kafka/KafkaConfiguration.java
@@ -2,7 +2,8 @@ package com.lovecws.mumu.kafka;
 
 public class KafkaConfiguration {
 
-    public static final String BOOTSTRAP_SERVERS_CONFIG="192.168.0.22:9092";
+    //public static final String BOOTSTRAP_SERVERS_CONFIG="192.168.0.22:9092";
+    public static final String BOOTSTRAP_SERVERS_CONFIG="localhost:9092";
 
     public static final String KEY_DESERIALIZER_CLASS_CONFIG="org.apache.kafka.common.serialization.IntegerDeserializer";
     public static final String VALUE_DESERIALIZER_CLASS_CONFIG="org.apache.kafka.common.serialization.StringDeserializer";
